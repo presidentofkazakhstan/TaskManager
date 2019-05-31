67abc746d580f00fb175d0ff710f2fcabd951836
Тынышбаев Алибек Алмасулы <@corp.itstep>
2019-05-31 21:17:35 +0600

    first commit

---
+++ TaskManager/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
+
+namespace TaskManager
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            taskManager.ItemsSource = Process.GetProcesses();
+        }
+
+        private void ButtonKillClick(object sender, RoutedEventArgs e)
+        {
+            foreach (var processTaskManager in Process.GetProcesses())
+            {
+                if ((taskManager.SelectedItem as Process).Id == processTaskManager.Id)
+                {
+                    try
+                    {
+                        processTaskManager.Kill();
+                    }
+                    catch (Exception)
+                    {
+                        MessageBox.Show("Нет Доступа, не хватает прав");
+                    }                   
+                }
+            }
+        }
+    }
+}
